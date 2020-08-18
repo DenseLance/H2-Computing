6827c7b957bf03036cef51d927b9cf8b98264d1f
DenseLance <denselance@gmail.com>
2020-08-18 10:35:38 +0800

    added README

--- Web App Programming/intro.py
+++ Web App Programming/intro.py
@@ -1,1 +1,4 @@
 # HTML/CSS (client side) <--- Python: jinja ---> Python: flask (server side) <--- Python: sqlite 3 ---> DB Browser
+# type python in terminal (CTRL + `) to enter python shell
+# type exit() to exit python shell
+# type pip list to check the libraries that are installed
