e7f6fa60ad7787e4b0d4f0b6d58509e95b679cf1
DenseLance <denselance@gmail.com>
2020-08-19 11:36:07 +0800

    edited web app

--- Web App Programming/app.py
+++ Web App Programming/app.py
@@ -7,15 +7,15 @@ from flask import Flask, render_template
 app = Flask(__name__) # __name__ is __main__; uses current file name to create a flask application
 
 @app.route("/") # define behaviour for route directory/uri
-@app.route("/index")
-@app.route("/index/")
+@app.route("/index/") # add trailing slash is better
 def index():
     return render_template("index.html")
 
-@app.route("/about")
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/about/<name>")
+def about(name):
+    class_ = "20J13"
+    age = 16
+    return render_template("about.html", class_ = class_, age = age, name = name)
 
 if __name__ == "__main__": # if app.py is run, then the application would run
     app.run(debug = True) # cross-imports would not run the application
