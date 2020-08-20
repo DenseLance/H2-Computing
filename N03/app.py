27fa9273a6003e17f28cc49140d5fef1039f7350
DenseLance <denselance@gmail.com>
2020-08-20 15:09:02 +0800

    updated N03

--- N03/app.py
+++ N03/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template("index.html")
 
 @app.route("/student/")
 def student():
@@ -13,6 +13,15 @@ def student():
 @app.route("/teacher/")
 def teacher():
     return render_template("teacher.html")
+
+@app.route('/display/<role>')
+def display(role):
+    if role == 'student': # search student function, print url for student (url_for)
+        return redirect(url_for('student')) # redirect /display/student to /student
+    elif role == 'teacher':  # search student function, print url for teacher
+        return redirect(url_for('teacher'))  # redirect /display/teacher to /teacher
+    else:
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug = True)
