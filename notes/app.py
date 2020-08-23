51831c0e03dc1464d5c94ed11137caa97f31c29f
DenseLance <denselance@gmail.com>
2020-08-23 17:44:34 +0800

    finished up navigation bar for notes

---
+++ notes/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+#app.route("/")
+#def home():
+    #return render_template("test.html")
+
+@app.route("/")
+@app.route("/home/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/resources/")
+def resources():
+    return render_template("resources.html")
+
+@app.route("/help/")
+def help():
+    return render_template("help.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
